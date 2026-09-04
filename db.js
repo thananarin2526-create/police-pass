c7d3550ff04ff809f7508f462a60c56c4b323556
thananarin2526-create <thananarin2526@gmail.com>
2026-09-04 13:18:51 +0700

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-
-const {Pool}=require('pg');
-const isProd=process.env.NODE_ENV==='production';
-const pool=new Pool({
-  connectionString:process.env.DATABASE_URL,
-  ssl:isProd && process.env.PGSSL_DISABLE!=='true' ? {rejectUnauthorized:false} : undefined
-});
-async function q(t,p=[]){return pool.query(t,p)}
-module.exports={pool,q};
