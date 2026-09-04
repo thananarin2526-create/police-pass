c512e845a6ad1f11ba20dce5b5d3a3bc9a07107f
thananarin2526-create <thananarin2526@gmail.com>
2026-09-04 13:14:38 +0700

    Delete services directory

--- services/security.js
+++
@@ -1,7 +0,0 @@
-
-const jwt=require('jsonwebtoken'),bcrypt=require('bcryptjs');
-function sign(u){return jwt.sign({id:u.id,role:u.role,name:u.name},process.env.JWT_ACCESS_SECRET,{expiresIn:'12h'})}
-function verify(t){return jwt.verify(t,process.env.JWT_ACCESS_SECRET)}
-function hp(p){return bcrypt.hashSync(p,12)}
-function cp(p,h){return bcrypt.compareSync(p,h)}
-module.exports={sign,verify,hp,cp};
